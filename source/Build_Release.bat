6f4e19543e3c32eeafec2b8f2398a1c8b9ef086a
Stefan <devnull@localhost>
2017-12-07 17:53:54 +0100

    Upgraded Deployment (Task #2109)

---
+++ source/Build_Release.bat
@@ -0,0 +1,2 @@
+pyinstaller --distpath "I:\RnD\PyD Python Framework\PE - Preset Editor\Release Repository" preset_editor.spec
+pause
--- source/Script2Release.bat
+++
@@ -1,2 +0,0 @@
-pyinstaller --name Preset_Editor_v1.00.exe --distpath "I:\RnD\PyD Python Framework\PE - Preset Editor\Release Repository\V1.00" --onefile --console preset_editor.spec
-pause
