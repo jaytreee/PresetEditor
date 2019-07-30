f6407e800d9fbd451c586b6bec3a7ef533b3098f
Stefan <devnull@localhost>
2019-07-30 11:36:46 +0200

    Updated Build Environment

--- Build_Release.bat
+++ Build_Release.bat
@@ -1,2 +1,3 @@
+set Path=%Path%;C:\Program Files (x86)\Windows Kits\10\Redist\ucrt\DLLs\x64;C:\Program Files (x86)\Microsoft SDKs\NuGetPackages\Microsoft.NETCore.Windows.ApiSets-x64\1.0.0\runtimes\win7-x64\native
 pyinstaller preset_editor.spec
 pause
